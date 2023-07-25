9985031938d6e64fbb91a4185203024112986a4d
Seth Seneca <sethseneca@Seths-Laptop.local>
2023-07-25 16:50:55 -0700

    July 25th - Website Version 1.1

--- bracket.py
+++ bracket.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('bracket.html')
+    return render_template('TOURNAMENTS/NCAA/NCAACHAMPIONSHIPS/2024/125LBS.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
